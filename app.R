9f8ac7fd79e112ef8ac5f53b9dca67f5d967f2cc
Øyvind Løvold Busk <oyvindbusk@gmail.com>
2022-03-31 09:47:52 +0200

    Add files via upload

---
+++ app.R
@@ -0,0 +1,196 @@
+library(shiny)
+library(DT)
+library(tidyverse)
+library(stringr)
+
+
+# appDir = "S:/analysis/dev/2022/wordl/wordlesolver"
+# rsconnect::deployApp(appDir)
+
+
+# Last inn ordbok med fembokstavers ord
+github_csv <- "https://raw.githubusercontent.com/oyvindbusk/wordlebundle/main/fiveletterwords.csv"
+#fivel_words <- read_delim(github_csv, delim=",",col_select = 'Word')
+fivel_words <- read_delim(github_csv, delim=",",col_select = 'Word')
+
+# Funksjoner:
+# Function filter words containing letter
+filter_included <- function(included_letters, data) {
+    # finn alle ord som inneholder den ene eller den andre
+    out_data <- data %>% filter(str_detect(string = Word, pattern = substring(included_letters,1,1), negate = FALSE))
+    if (nchar(included_letters) > 1) {
+        for (i in strsplit(included_letters, "")[[1]]) {
+            out_data <- out_data %>% filter(str_detect(string = Word, pattern = i, negate = FALSE))
+        }
+    }
+    return(out_data)
+}
+
+# Function filter words NOT containing letter
+filter_excluded <- function(excluded_letters, data) {
+    # finn alle ord som inneholder den ene eller den andre
+    out_data <- data %>% filter(str_detect(string = Word, pattern = substring(excluded_letters,1,1), negate = TRUE))
+    if (nchar(excluded_letters) > 1) {
+        for (i in strsplit(excluded_letters, "")[[1]]) {
+            out_data <- out_data %>% filter(str_detect(string = Word, pattern = i, negate = TRUE))
+        }
+    }
+    return(out_data)
+}
+
+
+filter_include_position <- function(position, ltr, data) {
+    return(data %>% filter(substr(Word, position,position) == ltr))
+}
+
+filter_exclude_position <- function(position, ltr, data) {
+    for (i in strsplit(ltr, "")[[1]]) {
+        data <- data %>% filter(substr(Word, position,position) != i)
+    }
+    return(data)
+}
+
+
+
+
+combine_filters <- function(included_letters, excluded_letters, incl_1, incl_2, incl_3, incl_4, incl_5, excl_1,excl_2,excl_3,excl_4,excl_5, data) {
+    if (nchar(included_letters) != 0) {
+        data <- filter_included(included_letters, data)
+    }
+    if (nchar(excluded_letters) != 0) {
+        data <- filter_excluded(excluded_letters, data)
+    }
+    if (nchar(incl_1) != 0) {
+        data <- filter_include_position(1, incl_1, data)
+    }
+    if (nchar(incl_2) != 0) {
+        data <- filter_include_position(2, incl_2, data)
+    }
+    if (nchar(incl_3) != 0) {
+        data <- filter_include_position(3, incl_3, data)
+    }
+    if (nchar(incl_4) != 0) {
+        data <- filter_include_position(4, incl_4, data)
+    }
+    if (nchar(incl_5) != 0) {
+        data <- filter_include_position(5, incl_5, data)
+    }
+    if (nchar(excl_1) != 0) {
+        data <- filter_exclude_position(1, excl_1, data)
+    }
+    if (nchar(excl_2) != 0) {
+        data <- filter_exclude_position(2, excl_2, data)
+    }
+    if (nchar(excl_3) != 0) {
+        data <- filter_exclude_position(3, excl_3, data)
+    }
+    if (nchar(excl_4) != 0) {
+        data <- filter_exclude_position(4, excl_4, data)
+    }
+    if (nchar(excl_5) != 0) {
+        data <- filter_exclude_position(5, excl_5, data)
+    }
+    return(data)
+}
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Øyvinds wordle-løser"),
+ 
+        # Show a plot of the generated distribution
+        mainPanel(
+            fluidRow(
+                column(3,textInput(inputId = "included_letters",
+                      label = "Inkluderte bokstaver:",
+                      value = "",
+                      width = "200px")),
+                column(3,textInput(inputId = "excluded_letters",
+                      label = "Ekskluderte bokstaver:",
+                      value = "",
+                      width = "200px")),
+                
+                
+            ),
+            h4("Bokstaver tilstede i posisjon x: (kun en)"),
+            fluidRow(
+                column(2,textInput(inputId = "included_1",
+                                   label = "Tilstede posisjon 1:",
+                                   value = "",
+                                   width = "100px")),
+                column(2,textInput(inputId = "included_2",
+                                   label = "Tilstede posisjon 2:",
+                                   value = "",
+                                   width = "100px")),
+                column(2,textInput(inputId = "included_3",
+                                   label = "Tilstede posisjon 3:",
+                                   value = "",
+                                   width = "100px")),
+                column(2,textInput(inputId = "included_4",
+                                   label = "Tilstede posisjon 4:",
+                                   value = "",
+                                   width = "100px")),
+                column(2,textInput(inputId = "included_5",
+                                   label = "Tilstede posisjon 5:",
+                                   value = "",
+                                   width = "100px"))
+                
+            ),
+            h4("Bokstaver IKKE tilstede i posisjon x: (mulig med mer enn en bokstav (uten mellomrom eller komma))"),
+            fluidRow(
+                column(2,textInput(inputId = "excluded_1",
+                                   label = "Ikke tilstede posisjon 1:",
+                                   value = "",
+                                   width = "100px")),
+                column(2,textInput(inputId = "excluded_2",
+                                   label = "Ikke tilstede posisjon 2:",
+                                   value = "",
+                                   width = "100px")),
+                column(2,textInput(inputId = "excluded_3",
+                                   label = "Ikke tilstede posisjon 3:",
+                                   value = "",
+                                   width = "100px")),
+                column(2,textInput(inputId = "excluded_4",
+                                   label = "Ikke tilstede posisjon 4:",
+                                   value = "",
+                                   width = "100px")),
+                column(2,textInput(inputId = "excluded_5",
+                                   label = "Ikke tilstede posisjon 5:",
+                                   value = "",
+                                   width = "100px")),
+            ),
+            br(),
+            DT::dataTableOutput("mytable"),
+            br(),
+            fluidRow(actionButton("show", "Trykk for Ann Gøril-sitat!!")),
+            br(),
+            tagList("Koden ligger her:", "https://github.com/oyvindbusk/wordlebundle"),
+            br()
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    output$mytable = DT::renderDataTable({
+        combine_filters(tolower(input$included_letters), tolower(input$excluded_letters), tolower(input$included_1), tolower(input$included_2), tolower(input$included_3), tolower(input$included_4), tolower(input$included_5), tolower(input$excluded_1), tolower(input$excluded_2), tolower(input$excluded_3), tolower(input$excluded_4), tolower(input$excluded_5), fivel_words)
+    },options = list(searching = FALSE))
+    #output$text <- renderText({ input$included_letters })
+    
+    observeEvent(input$show, {
+        showModal(modalDialog(
+            title = "Neida...",
+            "Det skarru få sleppe...",
+            easyClose = TRUE,
+            footer = NULL
+        ))
+    })
+    
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
